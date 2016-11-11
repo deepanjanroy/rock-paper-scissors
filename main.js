b0dfaf2f33b1aa754332c95cb51b67c7452fbd79
Deepanjan Roy <dproy@google.com>
2016-11-10 22:59:20 -0500

    ✌

---
+++ main.js
@@ -0,0 +1,106 @@
+document.addEventListener('DOMContentLoaded', function() {
+  function ridiculousDelay(delaySeconds) {
+    var now = performance.now();
+    while (performance.now() < now + delaySeconds) {}
+  }
+
+  function installHandlers() {
+    console.log("Installing handlers");
+    var rpsButton = document.querySelector('#rpsButton');
+    var rpsResult = document.querySelector('#rpsResult');
+    var gameInProgress = false;
+
+    function playRPS() {
+      if (gameInProgress) return;
+      gameInProgress = true;
+      var plays = ['Rock', 'Paper', 'Scissor'];
+      var playsEmoji = ['✊', '✋', '✌'];
+
+      var rpsProgress = (currentIndex) => {
+        if (currentIndex < 2) {
+          rpsResult.classList.remove('emoji-font');
+          rpsResult.innerText = plays[currentIndex] + '...';
+          setTimeout(() => rpsProgress(currentIndex + 1), 400);
+        } else {
+          rpsResult.classList.add('emoji-font');
+          rpsResult.innerText = playsEmoji[Math.floor(Math.random() * 3)];
+          gameInProgress = false;
+        }
+      };
+
+      setTimeout(() => rpsProgress(0), 400);
+    }
+
+    rpsButton.onclick = playRPS;
+  }
+
+  function installHandlerWithDelay(delayMs) {
+    var dclTime = performance.now();
+    var handlerInterval = setInterval(() => {
+      var currentTime = performance.now();
+      var statusDiv = document.querySelector('#handlerStatus');
+      if (currentTime > dclTime + delayMs) {
+        installHandlers();
+        statusDiv.innerText = 'Handlers installed';
+        clearInterval(handlerInterval);
+      } else {
+        var remaining = ((dclTime + delayMs - currentTime) / 1000);
+        var remainingFixed = remaining.toFixed(2);
+        statusDiv.innerText = `Installing handlers in ${remainingFixed} seconds`;
+      }
+    });
+  }
+
+  installHandlerWithDelay(5000);
+
+  setTimeout(() => ridiculousDelay(500), 0);
+  setTimeout(() => ridiculousDelay(500), 0);
+}, false);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
